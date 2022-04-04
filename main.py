b4d5620e399db38fcb13eec328959183241a371f
Матвей <cktkutxd@gmail.com>
2022-04-04 18:19:55 +0300

    first main page commit

--- main.py
+++ main.py
@@ -15,7 +15,18 @@ def main():
 
 @app.route("/")
 def header():
-    return render_template("base.html")
+    all = {}
+    all["title"] = "Парк Горького"
+    all["text"] = "Парк, подходящий как и для неспешных прогулой возле Москвы реки, так и для активного отдыха: в парке есть качели, даже верёвочный городок. В солгечные дни здесь солнечно а в пасмурные естьь гру укрыться от дождя. Всем советую"
+    all["photo"] = "/static/img/photo_park.jpg"
+    all["value"] = "0 - 5000(₽)"
+    all["turnout"] = "неограниченно, желательно компанией"
+    return render_template("main_page.html")
+
+
+@app.route("/choose_place")
+def choose_place():
+    return "yep"
 
 
 if __name__ == "__main__":
